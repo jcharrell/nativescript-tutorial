81a02a47e32e6592960b8408d2234d29e968d436
Chris Harrell <chris.harrell@sq1.com>
2016-05-14 11:56:00 +0000

    Test accessing UI components

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -1,7 +1,14 @@
 var frameModule = require('ui/frame');
+var viewModule = require('ui/core/view');
+var email;
+
+exports.loaded = function(args) {
+	var page = args.object;
+	email = viewModule.getViewById(page, 'email');
+};
 
 exports.signIn = function() {
-	alert('Signing In');
+	console.log(email.text);
 };
 
 exports.register = function() {
