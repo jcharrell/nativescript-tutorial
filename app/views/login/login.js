c3a2e53163bcdc8a0fd2bb603b2bdb6347ca0689
Chris Harrell <chris.harrell@sq1.com>
2016-05-15 16:41:00 +0000

    Implement the beginnings of the login vew model

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -1,14 +1,18 @@
 var frameModule = require('ui/frame');
-var viewModule = require('ui/core/view');
-var email;
+var Observable = require('data/observable').Observable;
+
+var user = new Observable({
+	email: "user@domain.com",
+	password: "password"
+});
 
 exports.loaded = function(args) {
 	var page = args.object;
-	email = viewModule.getViewById(page, 'email');
+	page.bindingContext = user;
 };
 
 exports.signIn = function() {
-	console.log(email.text);
+	console.log(user.email);
 };
 
 exports.register = function() {
