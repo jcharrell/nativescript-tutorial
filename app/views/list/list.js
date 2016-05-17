741dbf1907bd94739ba6c016bbf52f5b61d4c731
Chris Harrell <chris.harrell@sq1.com>
2016-05-17 09:33:25 -0500

    Implement a feature to add grocery items

--- app/views/list/list.js
+++ app/views/list/list.js
@@ -7,6 +7,7 @@ var page;
 var GroceryListViewModel = require("../../shared/view-models/grocery-list-view-model");
 var groceryList = new GroceryListViewModel([]);
 var pageData = new Observable({
+    grocery: "",
     groceryList: groceryList
 });
 
@@ -16,4 +17,28 @@ exports.loaded = function(args) {
 
     groceryList.empty();
     groceryList.load();
+};
+
+exports.add = function() {
+    // Check for empty submissions
+    if(pageData.get("grocery").trim() === "") {
+        dialogsModule.alert({
+            message: "Enter a grocery item",
+            okButtonText: "OK"
+        });
+        return;
+    }
+
+    // Dismiss the keyboard
+    page.getViewById("grocery").dismissSoftInput();
+    groceryList.add(pageData.get("grocery"))
+        .catch(function() {
+            dialogsModule.alert({
+                message: "An error occured while adding an item to your list",
+                okButtonText: "OK"
+            });
+        });
+
+    // Empty the input field
+    pageData.set("grocery", "");
 };
